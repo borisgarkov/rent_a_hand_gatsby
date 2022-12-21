b662242e4d7d22435e0d12c1e18d50359481f82f
Boris Garkov <boris.garkov@abv.bg>
2022-12-21 16:26:45 +0200

    adding google ver code to index.html

--- src/html.js
+++ src/html.js
@@ -14,6 +14,7 @@ export default function HTML(props) {
                 <script src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></script>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pace-js@latest/pace-theme-default.min.css"></link>
                 {props.headComponents}
+                <meta name="google-site-verification" content="G2Z4yaj4bhEJhBTWizKcsFib_dkx7WyBKfCwdIRaXk4" />
             </head>
             <body {...props.bodyAttributes}>
                 {props.preBodyComponents}
