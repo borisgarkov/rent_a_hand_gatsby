68c0d9e47a112e7185169257033c1467017bf961
Boris Garkov <boris.garkov@abv.bg>
2022-12-30 13:21:30 +0200

    update main menu

---
+++ src/components/NavigationBar/MainPageMenuDrawer.js
@@ -0,0 +1,96 @@
+import * as React from 'react';
+
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
+import PersonIcon from '@mui/icons-material/Person';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+import useScreenResolution from '../hooks/useScreenResolution';
+import { Link, navigate } from 'gatsby';
+
+const anchor = 'right';
+
+const MainMenuSection = ({ toggleDrawer }) => {
+    return (
+        <IconButton
+            id='menu-button'
+            aria-haspopup='true'
+            aria-controls='pages-menu'
+            aria-label='pages-menu'
+            onClick={toggleDrawer(anchor, true)}
+            sx={{
+                display: {
+                    xs: 'block',
+                    sm: 'none',
+                },
+                color: 'primary.main',
+            }}
+        >
+            <MenuIcon />
+        </IconButton>
+    )
+};
+
+export default function MainPageMenuDrawer({ menuItems, isUserLoggedIn, isUserSection }) {
+
+    const isMobile = useScreenResolution();
+    const [state, setState] = React.useState({ right: false, });
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (
+            event.type === 'keydown' &&
+            (event.key === 'Tab' || event.key === 'Shift')
+        ) {
+            return;
+        }
+
+        setState({ ...state, [anchor]: open });
+    };
+
+    const list = (anchor) => (
+        <Box
+            sx={{ width: isMobile ? 250 : 500 }}
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <List>
+                {
+                    menuItems.map((page) => (
+                        <Link to={page.path} key={page.title}>
+                            <ListItem disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        {page.icon}
+                                    </ListItemIcon>
+                                    <ListItemText primary={page.title} />
+                                </ListItemButton>
+                            </ListItem>
+                        </Link>
+                    ))
+                }
+            </List>
+        </Box>
+    );
+
+    return (
+        <Box key={anchor}>
+            <MainMenuSection toggleDrawer={toggleDrawer} />
+            <Drawer
+                anchor={anchor}
+                open={state[anchor]}
+                onClose={toggleDrawer(anchor, false)}
+            >
+                {list(anchor)}
+            </Drawer>
+        </Box>
+    );
+};
--- src/components/NavigationBar/Navigation.js
+++ src/components/NavigationBar/Navigation.js
@@ -5,6 +5,7 @@ import {
     Stack, Box, ThemeProvider, CssBaseline
 } from "@mui/material";
 
+import HomeIcon from '@mui/icons-material/Home';
 import LoginIcon from '@mui/icons-material/Login';
 import HowToRegIcon from '@mui/icons-material/HowToReg';
 
@@ -16,14 +17,19 @@ import { theme } from '../../utils/mainTheme';
 
 import Footer from '../CommonItems/Footer';
 import BackToTopButton from '../CommonItems/BackToTopButton';
+import MainPageMenuDrawer from './MainPageMenuDrawer';
 
 
 export default function Navigation(props) {
-
     const logo = cld.image('main page photos/Rent_A_Hand_D1_dwb3is').toURL();
     const isMobile = useScreenResolution();
 
     const menuPages = [
+        {
+            title: 'Начало',
+            icon: <HomeIcon fontSize="small" />,
+            path: '/',
+        },
         {
             title: 'Вход',
             icon: <LoginIcon fontSize="small" />,
@@ -39,7 +45,7 @@ export default function Navigation(props) {
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            <AppBar elevation={1} sx={{ backgroundColor: 'white' }}>
+            <AppBar elevation={0} sx={{ backgroundColor: 'white' }}>
                 <Toolbar sx={{
                     width: { xs: '100%' },
                     maxWidth: { md: '87%' },
@@ -50,26 +56,30 @@ export default function Navigation(props) {
                     <img src={logo} height={isMobile ? '20' : '30'} alt="logo img"
                         onClick={() => navigate('/')} style={{ cursor: 'pointer', }} />
 
-                    <ButtonGroup variant='' aria-label='button group'>
-                        {
-                            menuPages.map((page) => (
-                                <Link to={page.path} key={page.title}>
-                                    <Button variant='text' sx={{ color: '#666666', }}>
-                                        <Box sx={{
-                                            display: 'flex',
-                                            flexDirection: 'column',
-                                            alignItems: 'center',
-                                            fontSize: '12px',
-                                            margin: '0 20px',
-                                        }} >
-                                            {page.icon}
-                                            {page.title}
-                                        </Box>
-                                    </Button>
-                                </Link>
-                            ))
-                        }
-                    </ButtonGroup>
+                    {
+                        isMobile
+                            ? <MainPageMenuDrawer menuItems={menuPages} isUserSection={false} />
+                            : <ButtonGroup variant='' aria-label='button group'>
+                                {
+                                    menuPages.map((page) => (
+                                        <Link to={page.path} key={page.title}>
+                                            <Button variant='text' sx={{ color: '#666666' }}>
+                                                <Box sx={{
+                                                    display: 'flex',
+                                                    flexDirection: 'column',
+                                                    alignItems: 'center',
+                                                    fontSize: '12px',
+                                                    margin: '0 20px',
+                                                }} >
+                                                    {page.icon}
+                                                    {page.title}
+                                                </Box>
+                                            </Button>
+                                        </Link>
+                                    ))
+                                }
+                            </ButtonGroup>
+                    }
                 </Toolbar>
             </AppBar>
             <Toolbar id='navbar' />
