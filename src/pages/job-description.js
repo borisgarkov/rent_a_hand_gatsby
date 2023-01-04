116ac26fae58e3bf024a482eea588de288a71359
Boris Garkov <boris.garkov@abv.bg>
2023-01-04 13:42:46 +0200

    update jobs

--- src/pages/job-description.js
+++ src/pages/job-description.js
@@ -7,42 +7,16 @@ import { jobs, projects } from '../components/db-files/test-jobs';
 
 import WorkIcon from '@mui/icons-material/Work';
 import LocalOfferIcon from '@mui/icons-material/LocalOffer';
-import DesignServicesOutlinedIcon from '@mui/icons-material/DesignServicesOutlined';
 import BusinessCenterOutlinedIcon from '@mui/icons-material/BusinessCenterOutlined';
-
 import cld from "../services/getCloudinaryImages";
-import Navigation from "../components/NavigationBar/Navigation";
+import MainLayout from '../components/NavigationBar/MainLayout';
 
-const coverImage = cld.image('main page photos/main_pageworking_desk_m3ay3c').toURL();
-const job = jobs[3];
-
-const iconMetaDataStyle = {
-    color: '#666666',
-};
-
-const jobMetaData = [
-    {
-        icon: <WorkIcon sx={iconMetaDataStyle} />,
-        text: job.jobCategory,
-    },
-    {
-        icon: <LocalOfferIcon sx={iconMetaDataStyle} />,
-        text: job.salary,
-    },
-];
-
-const flexContainerJobMetaDataStyle = {
-    flexDirection: 'row',
-    alignItems: 'center',
-    columnGap: 1,
-};
-
-const SideAllJobsMenu = ({ jobs, title, icon }) => {
+const SideAllJobsMenu = ({ currentJob, jobs, title, icon }) => {
     return (
-        <Card sx={{ maxWidth: 433, alignSelf: 'flex-start', width: '100%' }}>
+        <Card sx={{ alignSelf: 'flex-start', width: '100%', marginBottom: '100px' }}>
             <Stack sx={{ flexDirection: 'row', alignItems: 'center', marginLeft: 2 }}>
                 {icon}
-                <CardHeader subheader={`${title} ${job.publisher}`} />
+                <CardHeader subheader={`${title} ${currentJob.publisher}`} />
             </Stack>
             {
                 jobs.map(job => (
@@ -63,21 +37,31 @@ const SideAllJobsMenu = ({ jobs, title, icon }) => {
 };
 
 export default function JobDescriptionPage() {
+    const coverImage = cld.image('main page photos/main_pageworking_desk_m3ay3c').toURL();
+    const job = jobs[3];
+
+    const jobMetaData = [
+        {
+            icon: <WorkIcon sx={{ color: '#666666' }} />,
+            text: job.jobCategory,
+        },
+        {
+            icon: <LocalOfferIcon sx={{ color: '#666666' }} />,
+            text: job.salary,
+        },
+    ];
+
     return (
-        <Navigation>
+        <MainLayout>
             <Box sx={{
                 display: 'flex',
-                flexDirection: { xs: 'column-reverse', md: 'row', },
+                flexDirection: 'column',
                 gap: 1,
-                justifyContent: 'center',
-                marginTop: { xs: 1, md: 5, },
-                marginBottom: 2,
+                paddingTop: 10,
+                margin: '0 auto',
+                maxWidth: { lg: '80%' }
             }}>
-                <Stack sx={{ gap: 2 }}>
-                    <SideAllJobsMenu jobs={jobs} title='Други обяви от' icon={<BusinessCenterOutlinedIcon />} />
-                    <SideAllJobsMenu jobs={projects} title='Други проекти от' icon={<DesignServicesOutlinedIcon />} />
-                </Stack>
-                <Card sx={{ maxWidth: 676 }}>
+                <Card sx={{ width: '100%' }}>
                     <CardHeader
                         title={job.title}
                         subheader={`${job.city}`}
@@ -89,11 +73,15 @@ export default function JobDescriptionPage() {
                         alt='card img'
                         sx={{ marginBottom: 2 }}
                     />
-
                     <CardContent>
                         {
                             jobMetaData.map(x => (
-                                <Stack key={x.text} sx={{ ...flexContainerJobMetaDataStyle }} marginBottom={2}>
+                                <Stack key={x.text} sx={{
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    columnGap: 1,
+                                    marginBottom: 2
+                                }}>
                                     {x.icon}
                                     <Typography variant="body2">{x.text}</Typography>
                                 </Stack>
@@ -102,18 +90,27 @@ export default function JobDescriptionPage() {
 
                         <Stack spacing={1} sx={{ marginTop: 5 }}>
                             <Typography variant="body2">Споделено от</Typography>
-                            <Stack spacing={2} sx={{ flexDirection: 'row', alignItems: 'flex-start', columnGap: 2 }}>
+                            <Stack spacing={2} sx={{
+                                flexDirection: 'row',
+                                alignItems: 'flex-start', columnGap: 2
+                            }}>
                                 <Avatar alt='profile img' />
                                 <Typography variant="body2">{job.publisher}</Typography>
                             </Stack>
                         </Stack>
-                        <Typography variant="body2" sx={{ margin: '16px 0', textAlign: 'justify', }}>
-                            {job.description}
+                        <Typography variant="body2" sx={{ margin: '16px 0' }}>
+                            {
+                                job.description
+                            }
                         </Typography>
                         <Button variant='contained'>Свържи се</Button>
                     </CardContent>
                 </Card>
+                <Stack sx={{ gap: 2 }}>
+                    <SideAllJobsMenu currentJob={job} jobs={jobs} title='Други обяви от'
+                        icon={<BusinessCenterOutlinedIcon />} />
+                </Stack>
             </Box>
-        </Navigation>
+        </MainLayout >
     )
-}
+};
